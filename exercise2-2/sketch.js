43103935b8d2dad8755b54ed38aea3ff854fb4b8
rcenti <vlt534@york.ac.uk>
2025-10-08 11:58:22 +0100

    done

---
+++ exercise2-2/sketch.js
@@ -0,0 +1,37 @@
+let fillColour;
+let x = 50;
+let y = 50;
+
+function setup() {
+
+    createCanvas(600, 600);
+    fillColour = color(random(255), random(255), random(255));
+}
+
+function draw() {
+background(220);
+    fill(fillColour);
+    rect(x, y, 500, 500);
+}
+
+function keyPressed() {
+    if (keyCode === UP_ARROW) {
+        y -= 5;
+    } 
+    if (keyCode === DOWN_ARROW) {
+        y += 5;
+    }
+    if (keyCode === LEFT_ARROW) {
+        x -= 5;
+    }
+    if (keyCode === RIGHT_ARROW) {
+        x += 5;
+    }
+
+}
+
+function mouseClicked() {
+    if (mouseX > x && mouseX < x + 500 && mouseY > y && mouseY < y + 500) {
+        fillColour = color(random(255), random(255), random(255));
+    }
+}
